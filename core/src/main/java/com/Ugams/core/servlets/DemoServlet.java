30379ca9f2c2d81c909e9f7feeffcc8b9c70e9e2
Pooja Narwade <pooja.narwade@ugamsolutions.com>
2021-11-18 13:40:48 +0530

    Test case added to Servlet

--- core/src/main/java/com/Ugams/core/servlets/DemoServlet.java
+++ core/src/main/java/com/Ugams/core/servlets/DemoServlet.java
@@ -3,8 +3,9 @@ package com.Ugams.core.servlets;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+
 import org.apache.sling.servlets.annotations.SlingServletPaths;
-import org.apache.sling.servlets.annotations.SlingServletResourceTypes;
+
 import org.osgi.service.component.annotations.Component;
 
 import javax.servlet.Servlet;
@@ -22,8 +23,10 @@ public class DemoServlet extends SlingAllMethodsServlet {
     protected void doGet(final SlingHttpServletRequest req, final SlingHttpServletResponse resp) throws IOException {
         int firstvalue = Integer.parseInt(req.getParameter("firstvalue"));
         int secondvalue = Integer.parseInt(req.getParameter("secondvalue"));
-        int total = firstvalue + secondvalue;
+        int total=firstvalue+secondvalue;
+
         resp.getWriter().print(total);
+
 
     }
 }
--- core/src/test/java/com/Ugams/core/servlets/DemoServletTest.java
+++ core/src/test/java/com/Ugams/core/servlets/DemoServletTest.java
@@ -1,28 +1,49 @@
 package com.Ugams.core.servlets;
 
-/*import com.google.common.cache.CacheLoader;
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletRequest;
+import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletResponse;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import javax.servlet.ServletException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
 @ExtendWith({AemContextExtension.class, MockitoExtension.class})
 
 class DemoServletTest {
-    private final AemContext aemContext=new AemContext();
 
+    private  DemoServlet demoServlet = new DemoServlet();
     @BeforeEach
-    void setUp() {
+    void setUp() throws Exception{
 
-        aemContext.load().json("/servlet.json","/content");
     }
+
 
     @Test
-    void doGet(AemContext aemContext)  {
+     void doGet(AemContext aemContext) throws  IOException {
+        MockSlingHttpServletRequest request = aemContext.request();
+        MockSlingHttpServletResponse response = aemContext.response();
+
+
+        Map<String, Object> parameters = new HashMap<String, Object>();
+
+        parameters.put("firstvalue", 3);
+        parameters.put("secondvalue", 4);
+        request.setParameterMap(parameters);
+        demoServlet.doGet(request,response);
+
+        assertEquals(7 , Integer.parseInt(response.getOutputAsString()));
     }
-}*/
+}
