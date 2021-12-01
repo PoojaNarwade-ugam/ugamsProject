b40a81e856026fdaec5747494122c19836e0c49b
Pooja Narwade <pooja.narwade@ugamsolutions.com>
2021-12-01 13:03:05 +0530

    added ServiceUserModel and ResolverUtils

---
+++ core/src/main/java/com/Ugams/core/models/BannerNav.java
@@ -0,0 +1,9 @@
+package com.Ugams.core.models;
+
+public interface BannerNav {
+    String getTitle();
+    String getNav1();
+    String getNav2();
+    String getNav1Path();
+    String getNav2Path();
+}
